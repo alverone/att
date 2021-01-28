a384434b575bd18358a345c8744b194ef8265b50
alverone <48132231+alverone@users.noreply.github.com>
2021-01-28 21:52:30 +0200

    Update and rename main35.js to main36.js

--- main36.js
+++ main36.js
@@ -82,7 +82,7 @@ $("#rangeSlider").on("input", function (e) {
     $(".additionalregprice").html(formPriceString(price));
     $(".additionalcreditsamount").html(parseInt($(this).val()) - 500);
     sliderVal = price;
-    $total.html(formPriceString(4000 + price + getData($eventAppPackageTotalRadios, "price") + getData($streamingPackageRadios, "price")));
+    $total.html(formPriceString(4000 + price + getData($(":radio[name='eventAppPackageTotal']:checked"), "price") + getData($streamingPackageRadios, "price")));
 });
 
 $("#rangeSlider")
@@ -112,7 +112,7 @@ $("#rangeSlider")
         $(".additionalregprice").html(formPriceString(price));
         $(".additionalcreditsamount").html(value - 500);
         sliderVal = price;
-        $total.html(formPriceString(4000 + price + getData($eventAppPackageTotalRadios, "price") + getData($streamingPackageRadios, "price")));
+        $total.html(formPriceString(4000 + price + getData($(":radio[name='eventAppPackageTotal']:checked"), "price") + getData($streamingPackageRadios, "price")));
     });
 
 $(".rangeSlider").on("input", function (e) {
